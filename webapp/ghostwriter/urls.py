c5197d9c6c2bdbae50f88380155addc455d5d92f
tatsuya1407 <us.asjdnm@gmail.com>
2017-10-21 16:52:55 +0900

    各view関数の追加、url.pyに追加

--- webapp/ghostwriter/urls.py
+++ webapp/ghostwriter/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, LectureView,LectureDetailView
 
 
 urlpatterns = [
-    url(r"^$", IndexView.as_view())
+    url(r"^$", IndexView.as_view(), name='index'),
+    url(r'^lecture/(?P<id>\d+)/$', LectureDetailView.as_view(), name='lecture'),
+    url(r'^lectures/$', LectureView.as_view(), name='lectures'),
 ]
--- webapp/ghostwriter/views.py
+++ webapp/ghostwriter/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView
-from .models import Image
+from django.views.generic import TemplateView,ListView
+from .models import Image,Lecture
 
 
 class IndexView(TemplateView):
@@ -8,4 +8,17 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context["object_list"] = Image.objects.filter(lecture__isnull=True).all()
+        return context
+
+class LectureView(ListView):
+    template_name = 'ghostwriter/lectures.html'
+    model = Lecture
+
+class LectureDetailView(TemplateView):
+    template_name = 'ghostwriter/lecture_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LectureDetailView, self).get_context_data(**kwargs)
+        lec_id = self.kwargs['id']
+        context['item'] = Lecture.objects.get(id=lec_id)
         return context
