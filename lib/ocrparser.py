5e9bddf7ae2ca9ac05c5faed0ddc7c8f76b8fa6f
AkiraMAEGAWA <maeaki112@gmail.com>
2017-10-21 22:53:35 +0900

    add docstring

--- lib/ocrparser.py
+++ lib/ocrparser.py
@@ -1,11 +1,18 @@
 
 class ocrparser():
+    '''
+    googleocrで画像から文字列を取得して,その文字列情報(resp)をパースする。
+    パースした文字列はリストで返す
+
+    :return spllited_strings:
+    '''
     def __init__(self, resp: dict):
         self.resp = resp
 
-    def get_ocr_strings(self, resp) -> [str]:
+    def get_ocr_strings(self, resp: dict) -> [str]:
         ocred_string = resp['textAnnotations'][0]['description']
         splitted_strings = ocred_string.split('/n')
 
         return splitted_strings
+
 
