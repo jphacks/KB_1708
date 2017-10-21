b5a46df474bb445f50ef47555b67ae951aed77e6
ppdg <pudding@mail.poyo.info>
2017-10-21 17:04:04 +0900

    [fix] lectureの詳細のviewが表示できない問題を修正

--- webapp/ghostwriter/urls.py
+++ webapp/ghostwriter/urls.py
@@ -4,6 +4,6 @@ from .views import IndexView, LectureView,LectureDetailView
 
 urlpatterns = [
     url(r"^$", IndexView.as_view(), name='index'),
-    url(r'^lecture/(?P<id>\d+)/$', LectureDetailView.as_view(), name='lecture'),
     url(r'^lectures/$', LectureView.as_view(), name='lectures'),
+    url(r'^lectures/(?P<id>\d+)/$', LectureDetailView.as_view(), name='lecture'),
 ]
