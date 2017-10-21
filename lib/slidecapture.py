9d59fc379ed10e1bf251826f8fd4485c8e7409a3
taxio <taxio.2029@gmail.com>
2017-10-21 13:10:18 +0900

    camera sample

--- lib/slidecapture.py
+++ lib/slidecapture.py
@@ -2,16 +2,17 @@ import cv2
 
 
 class SlideCapture:
-    def __init__(self, dev_id=0):
+    def __init__(self, dev_id: int=0):
         """
         カメラのIDを指定する．内臓カメラはだいたい0に設定されているので，webカメラを使いたい場合は1にする．
         :param dev_id: カメラのID
         """
         self.dev_id = dev_id
 
-    def camera_test(self, size=(800, 600)):
+    def camera_test(self, img_size=(800, 600)):
         print('start camera test.')
         print('press esc key for quit.')
+        print('press s key for save image')
 
         cap = cv2.VideoCapture(self.dev_id)
 
@@ -21,15 +22,17 @@ class SlideCapture:
 
             # フレームをリサイズ
             # sizeは例えば(800, 600)
-            if size is not None and len(size) == 2:
-                frame = cv2.resize(frame, size)
+            frame = cv2.resize(frame, img_size)
 
             # フレームを表示する
             cv2.imshow('camera capture', frame)
 
-            k = cv2.waitKey(1)  # 1msec待つ
-            if k == 27:  # escキーで終了
+            k = cv2.waitKey(1)                  # 1msec待つ
+            if k == 27:                         # escキーで終了
                 break
+            elif k == ord('s'):                 # 保存
+                cv2.imwrite('test_frame.jpg', frame)
+                print('image \'test_frame/jpg\' saved.')
 
         # キャプチャを解放する
         cap.release()
---
+++ samples/camera/camera_test.py
@@ -0,0 +1,4 @@
+import slidecapture
+
+cap = slidecapture.SlideCapture(dev_id=1)
+cap.camera_test(img_size=(800, 600))
