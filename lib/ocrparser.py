bb8eab5d508c945bf4538299e7202a8e707f72a9
AkiraMAEGAWA <maeaki112@gmail.com>
2017-10-22 00:19:17 +0900

    ocrした文字列からタブを除いた

--- lib/ocrparser.py
+++ lib/ocrparser.py
@@ -1,18 +1,17 @@
 
 class Ocrparser():
     '''
-    googleocrで画像から文字列を取得して,その文字列情報(resp)をパースする。
-    パースした文字列はリストで返す
+    googleocrで画像から文字列を取得して,その文字列情報(resp)をパースして返す
 
-    :return spllited_strings:
+    :return ocr_strings:
     '''
     def __init__(self, resp: dict):
         self.resp = resp
 
     def get_ocr_strings(self, resp: dict) -> [str]:
-        ocred_string = resp['textAnnotations'][0]['description']
-        splitted_strings = ocred_string.split('/n')
+        raw_ocr_string = resp['textAnnotations'][0]['description']
+        ocr_string = raw_ocr_string.replace('/n', '')
 
-        return splitted_strings
+        return ocr_string
 
 
