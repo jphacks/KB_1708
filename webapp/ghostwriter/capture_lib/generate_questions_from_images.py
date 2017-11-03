3a2f4d5162cd97ec90dd927806ee85733ea1f949
tatsuya1407 <us.asjdnm@gmail.com>
2017-11-03 22:11:35 +0900

    問題作成アルゴリズムの変更

--- webapp/ghostwriter/capture_lib/generate_questions_from_images.py
+++ webapp/ghostwriter/capture_lib/generate_questions_from_images.py
@@ -1,6 +1,5 @@
 from .googleocr import GoogleOCR
 from goolabs import GoolabsAPI
-from . import question_generator
 import json
 import random
 
@@ -31,14 +30,19 @@ class GoolabWrapper(object):
 
     def get_keywords_from_ocr_string(self, ocr_string):
         keywords = []
-        ret = self.goolab.entity(sentence=ocr_string)
+        ret = self.goolab.entity(sentence=ocr_string, class_filter=u"PSN|ORG|ART|DAT")
         for idx in range(len(ret['ne_list'])):
-            keywords.append(ret['ne_list'][idx][0])
+            key = (ret['ne_list'][idx][0], ret['ne_list'][idx][1])
+            keywords.append(key)
         return keywords
 
     def generate_selected_num_of_questions(self, keyword, num):
+        from . import question_generator
         q_gen = question_generator.QuestionGeneratorOfKeywords(keyword)
         all_questions = q_gen.create_questions_with()
-        for i in range(len(all_questions) - num):
-            del all_questions[random.randint(0, len(all_questions)-i)]
-        return all_questions
+        # for i in range(len(all_questions) - num):
+        #     print(all_questions)
+        #     print(i, len(all_questions)-1)
+        #     del all_questions[random.randint(0, len(all_questions)-1)]
+        selectes_q = random.sample(all_questions, num)
+        return selectes_q
