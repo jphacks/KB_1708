ebb2f46c9b10ab4f9188e20ad906e7e7f9bdfb13
ppdg <pudding@mail.poyo.info>
2017-11-11 22:22:55 +0900

    [update] フレームスキップオプションの追加

--- webapp/ghostwriter/management/commands/capture.py
+++ webapp/ghostwriter/management/commands/capture.py
@@ -1,14 +1,21 @@
 from __future__ import absolute_import
 from django.core.management.base import BaseCommand
-from slidecapture import SlideCapture, SlideCaptureError
+from ghostwriter.capture_lib.slidecapture import SlideCapture, SlideCaptureError
 
 
 class Command(BaseCommand):
     help = "Capturing slides"
 
+    def add_arguments(self, parser):
+        parser.add_argument('skip_frame_num',
+                            type=int,
+                            help='Skip frame number',
+                            default=3,
+                            nargs="?")
+
     def handle(self, *args, **options):
         try:
             with SlideCapture(0) as cap:
-                cap.monitor_slides("media/cache")
+                cap.monitor_slides("media/cache", options['skip_frame_num'])
         except SlideCaptureError:
             print("Couldn't open camera!!")
